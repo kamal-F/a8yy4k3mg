c7cfa837044e8a7373e3d3d6b0a01d05e0d8e9fb
Ulfah <Ulfah@DESKTOP-9631A86>
2016-04-06 18:03:14 +0700

    Nama: Siti Ulfah Naziah Kelas: D4TI1C NPM: 1144073

--- klien2.php
+++ klien2.php
@@ -1,16 +1,16 @@
 <?php
-$wsdl = "http://localhost/latihan/utswebservice/server/uts.wsdl";
+$wsdl = "http://localhost:8080/latihan/utswebserver/server/a8yy4k3mg.git/uts.wsdl";
 $client2 = new SoapClient ( $wsdl );
 
 $ws = $client2->getInfoMK ();
 $wsout = $ws->MK;
-
 echo '<p>';
 echo 'List mata kuliah';
 echo '</p>';
 echo '<table>';
 echo '<thead><td>Nama Mk</td><td>SKS</td></thead>';
-foreach ( $wsout as $val ) {
+foreach ( $wsout as $val ) 
+{
 	echo '<tr><td>' . $val->namaMK . '</td><td>' . $val->SKS . '</td></tr>';
 }
 echo '</table>';
