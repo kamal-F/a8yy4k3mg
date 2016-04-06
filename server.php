558d8ca027cd580fe0f14f1c1777bf2de1846c7b
yandarizky <yrizky35@gmail.com>
2016-04-06 18:05:31 +0700

    yanda rizky prasetiya 1144004

---
+++ server.php
@@ -0,0 +1,94 @@
+<?php
+// This is the API, 2 possibilities: show the app list or show a specific app by id.
+// This would normally be pulled from a database but for demo purposes, I will be hardcoding the return values.
+// https://trinitytuts.com/build-first-web-service-php/
+function mhs($nama, $nim)
+{
+  //normally this info would be pulled from a database.
+  //build JSON array
+  $nama = $_POST{'nama'};
+  $nim = $_POST{'nim'};
+  $data = "Hello, Selamat Datang ".$nama. " NIM Anda ".$nim; 
+      
+  return $data;
+}
+function tambah($prm1, $prm2)
+{
+  //normally this info would be pulled from a database.
+  //build JSON array
+  $jumlah = $prm1+$prm2; 
+      
+  return $jumlah;
+}
+ function get_app_by_id($id)
+{
+  $app_info = array();
+  // normally this info would be pulled from a database.
+  // build JSON array.
+  switch ($id){
+    case 1:
+      $app_info = array("Kode Barang" => "001", "Nama Barang" => "Besi", "Harga" => "20000"); 
+      break;
+    case 2:
+      $app_info = array("app_name" => "Audio Countdown", "app_price" => "Free", "app_version" => "1.1");
+      break;
+    case 3:
+      $app_info = array("app_name" => "The Tab Key", "app_price" => "Free", "app_version" => "1.2");
+      break;
+    case 4:
+      $app_info = array("app_name" => "Music Sleep Timer", "app_price" => "Free", "app_version" => "1.9");
+      break;
+  }
+  return $app_info;
+} 
+$possible_url = array("get_app_list", "get_app", "mhs", "tambah");
+$value = "An error has occurred";
+if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url))
+{
+  switch ($_GET["action"])
+    {
+      case "get_app_list":
+        $value = get_app_list();
+        break;
+	 case "tambah":
+        if (isset($_GET["prm1"]) && ($_GET["prm2"]))
+         $value = tambah($_GET["prm1"],$_GET["prm2"]);
+       
+       break;
+       case "mhs":
+        if (isset($_GET["nama"]) && ($_GET["nim"]))
+         $value = mhs($_GET["nama"],$_GET["nim"]);
+       
+       break;
+        
+        
+      case "get_app":
+        if (isset($_GET["id"]))
+          $value = get_app_by_id($_GET["id"]);
+        else
+          $value = "Missing argument";
+        break;
+    }
+}
+// kamal
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+	$json = file_get_contents('php://input');
+	$obj = json_decode($json);
+	$value =  mhs($_POST{"nama"}, $_POST{"nim"});
+}
+if($_SERVER['REQUEST_METHOD'] == "PUT"){
+	$json = file_get_contents('php://input');
+	$obj = json_decode($json);	
+	$id= $_SERVER['HTTP_ID'];
+	
+	$value = "berhasil diupdate ".$id." ".$obj->{'nama'};
+}
+if($_SERVER['REQUEST_METHOD'] == "DELETE"){
+	$id= $_SERVER['HTTP_ID'];	
+	$value = "dihapus ". $id;
+	
+}
+header('Content-type: application/json');
+//return JSON array
+exit(json_encode($value));
+?>
