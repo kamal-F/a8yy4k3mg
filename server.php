7b0e6e61f7eaff4833d6e2207af9e9b1f6d915b7
gilan <gilan@GILANG>
2016-04-06 18:56:29 +0700

    Gilang Romadhanu Tartila 1144033 D4 TI 2C

---
+++ server.php
@@ -0,0 +1,20 @@
+<?php
+
+class MyWs{
+	
+	function getInfoMK(){
+		$re->PPKN='4';
+		$re->Bahasa_Indonesia='2';
+		$re->Tataboga='2';
+				
+		return $re;		
+	}
+}
+	
+//ini_set("soap.wsdl_cache_enabled", 0);
+$server = new SoapServer("server/uts.wsdl");
+$server->setClass("MyWs");
+$server->handle();
+
+
+?>
